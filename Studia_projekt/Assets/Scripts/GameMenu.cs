6fea6323d6a4aaf13edb1b2d53355b7e032e4220
Shiloon <supershiloon@gmail.com>
2022-01-16 15:22:59 +0100

    Pause menu

---
+++ Studia_projekt/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+
+    private void Start()
+    {
+        ResumeGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) == true)
+        {
+            if (Time.timeScale == 0.0f)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+
+        }
+    }
+
+    private void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    private void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void OnResumeClicked()
+    {
+        ResumeGame();
+    }
+
+    public void OnBackToMenuClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
