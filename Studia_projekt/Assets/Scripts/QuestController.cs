c692ad4f4b51a73baf9d8365a451af536fed4e25
Shiloon <supershiloon@gmail.com>
2022-01-22 18:57:26 +0100

    QuestController part 1

---
+++ Studia_projekt/Assets/Scripts/QuestController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestController : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triger enter" + other.gameObject.name);
+        RobotNPC npc = other.gameObject.GetComponent<RobotNPC>();
+
+        if (npc != null)
+        {
+            npc.ShowUI();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RobotNPC npc = other.gameObject.GetComponent<RobotNPC>();
+
+        if (npc != null)
+        {
+            npc.HideUI();
+        }
+
+    }
+}
---
+++ Studia_projekt/Assets/Scripts/RobotNPC.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotNPC : MonoBehaviour
+{
+    public GameObject uiPanel;
+
+    public void ShowUI()
+    {
+        uiPanel.SetActive(true);
+    }
+
+    public void HideUI()
+    {
+        uiPanel.SetActive(false);
+    }
+}
